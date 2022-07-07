c67154a8677633c6cd2e6fd0117c57113961235d
Johan Solbakken <johan.solbakken@qt.io>
2022-07-07 15:55:42 +0200

    Add shiny-metal example

---
+++ src/examples/shiny-metal/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+
+#include <raytracer.h>
+
+raytracer::ref<raytracer::Hittable> createWorld() {
+	using namespace raytracer;
+
+	auto material_ground = createRef<Lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = createRef<Lambertian>(color(0.7, 0.3, 0.3));
+	auto material_left   = createRef<Metal>(color(0.8, 0.8, 0.8));
+	auto material_right  = createRef<Metal>(color(0.8, 0.6, 0.2));
+
+	auto world = createRef<HittableList>();
+	world->add<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground);
+	world->add<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center);
+	world->add<Sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left);
+	world->add<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right);
+
+	return world;
+}
+
+int main(int argc, char** argv)
+{
+	std::string filename = "image.ppm";
+
+	if (argc != 1)
+	{
+		filename = argv[1];
+	}
+
+	std::ofstream file(filename, std::ios::out);
+	file.clear();
+
+	// Image
+	raytracer::Image image{};
+	image.aspect_ratio = 16.0 / 9.0;
+	image.image_width = 400;
+
+	// World
+	auto world = createWorld();
+
+	// Camera
+	raytracer::CameraSpecification cameraSpec;
+	cameraSpec.aspect_ratio = image.aspect_ratio;
+	cameraSpec.viewport_height = 2.0f;
+	cameraSpec.focal_length = 1.0;
+
+	auto camera = raytracer::createRef<raytracer::Camera>(cameraSpec);
+
+	// render
+	raytracer::RendererSpecification rendererSpecification{.buffer = file};
+	rendererSpecification.samplesPerPixel = 32;
+	rendererSpecification.recursionDepth = 50;
+
+	raytracer::Renderer renderer(rendererSpecification);
+	renderer.render(image, world, camera);
+
+	file.close();
+
+	return 0;
+}
