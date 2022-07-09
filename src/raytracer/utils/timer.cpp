c25014018ebbe1841794f0352846fe083b9978f1
Johan Solbakken <johan.hvaler@gmail.com>
2022-07-09 20:32:24 +0200

    Add timer for profiling

---
+++ src/raytracer/utils/timer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Johan Solbakken on 09/07/2022.
+//
+
+#include "timer.h"
---
+++ src/raytracer/utils/timer.h
@@ -0,0 +1,70 @@
+//
+//	 Adapted from Walnut (https://github.com/TheCherno/Walnut/blob/master/Walnut/src/Walnut/Timer.h)
+//
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <utility>
+#include <functional>
+
+namespace raytracer
+{
+	class Timer
+	{
+	public:
+		Timer()
+		{
+			reset();
+		}
+
+		void reset()
+		{
+			m_start = std::chrono::high_resolution_clock::now();
+		}
+
+		float elapsed()
+		{
+			return (float)std::chrono::duration_cast<std::chrono::nanoseconds>(
+					std::chrono::high_resolution_clock::now() - m_start).count() * 0.001f * 0.001f * 0.001f;
+		}
+
+		float elapsedMillis()
+		{
+			return elapsed() * 1000.0f;
+		}
+
+	private:
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
+	};
+
+	struct ScopedTimerResult
+	{
+		std::string name;
+		float time;
+	};
+
+	class ScopedTimer
+	{
+	public:
+		using CallbackFn = std::function<void(ScopedTimerResult)>;
+
+		explicit ScopedTimer(std::string name, CallbackFn callback)
+				: m_name(std::move(name)), m_callback(std::move(callback))
+		{
+		}
+
+		~ScopedTimer()
+		{
+			float time = m_timer.elapsedMillis();
+			m_callback({ m_name, time });
+		}
+
+	private:
+		std::string m_name;
+		Timer m_timer;
+		CallbackFn m_callback;
+	};
+}
