77790420fefd29d26978ee6e54b82f21f63f2c99
Johan Solbakken <johan.hvaler@gmail.com>
2022-07-10 22:02:36 +0200

    Add differentialgeometry class

---
+++ src/raytracer/geometry/differentialgeometry.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Johan Solbakken on 09/07/2022.
+//
+
+#include "differentialgeometry.h"
+
+namespace raytracer
+{
+
+	DifferentialGeometry::DifferentialGeometry()
+	{
+		u = v = 0.;
+		shape = nullptr;
+	}
+
+	DifferentialGeometry::DifferentialGeometry(const Point3& point, const Vector3& dpdu, const Vector3& dpdv,
+			const Vector3& dndu, const Vector3& dndv, float uu, float vv, const Shape* s)
+			: point(point), dPointdu(dpdu), dPointdv(dpdv), dNormaldu(dndu), dNormaldv(dndv)
+	{
+		// Initialize DifferentialGeometry from parameters
+		normalNormalized = glm::normalize(glm::cross(dpdu, dpdv));
+		u = uu;
+		v = vv;
+		shape = s;
+
+		// Adjust normal based on orientation and handedness
+		if (shape && (shape->reverseOrientation() ^ shape->transformSwapHandedness()))
+			normalNormalized *= -1.0f;
+	}
+}
---
+++ src/raytracer/geometry/differentialgeometry.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "utils/glmutils.h"
+#include "shapes/shape.h"
+
+namespace raytracer
+{
+	struct DifferentialGeometry
+	{
+		DifferentialGeometry();
+		DifferentialGeometry(const Point3& point, const Vector3& dpdu, const Vector3& dpdv,
+				const Vector3& dndu, const Vector3& dndv, float uu, float vv, const Shape* s);
+
+		Point3 point{};
+		Vector3 normalNormalized{};
+		float u, v;
+		const Shape* shape;
+
+		Vector3 dPointdu{}, dPointdv{}; // leibniz notation
+		Vector3 dNormaldu{}, dNormaldv{};
+	};
+}
+
