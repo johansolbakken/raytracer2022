e8d3450432c131963c2893dc7c65fb7d08c0f7d3
Johan Solbakken <johan.hvaler@gmail.com>
2022-07-10 21:58:45 +0200

    Add point constructor to aabb

--- src/raytracer/accelerator/aabb.cpp
+++ src/raytracer/accelerator/aabb.cpp
@@ -21,32 +21,52 @@ namespace raytracer
 		return true;
 	}
 
-	Aabb::Aabb(const point3& a, const point3& b)
+	Aabb::Aabb(const Point3& a, const Point3& b)
 	{
 		m_minimum = a;
 		m_maximum = b;
 	}
 
-	point3 Aabb::minimum() const
+	Point3 Aabb::minimum() const
 	{
 		return m_minimum;
 	}
 
-	point3 Aabb::maximum() const
+	Point3 Aabb::maximum() const
 	{
 		return m_maximum;
 	}
 
 	Aabb Aabb::surroundingBox(const Aabb& box0, const Aabb& box1)
 	{
-		point3 small(std::fmin(box0.m_minimum.x, box1.m_minimum.x),
+		Point3 small(std::fmin(box0.m_minimum.x, box1.m_minimum.x),
 				std::fmin(box0.m_minimum.y, box1.m_minimum.y),
 				std::fmin(box0.m_minimum.z, box1.m_minimum.z));
 
-		point3 big(fmax(box0.m_maximum.x, box1.m_maximum.x),
+		Point3 big(fmax(box0.m_maximum.x, box1.m_maximum.x),
 				fmax(box0.m_maximum.y, box1.m_maximum.y),
 				fmax(box0.m_maximum.z, box1.m_maximum.z));
 
 		return { small, big };
+	}
+
+	Aabb Aabb::unionPt(const Aabb& box, const Point3& pt)
+	{
+		Aabb b = box;
+
+		b.m_minimum.x = std::min(box.m_minimum.x, pt.x);
+		b.m_minimum.y = std::min(box.m_minimum.y, pt.y);
+		b.m_minimum.z = std::min(box.m_minimum.z, pt.z);
+		b.m_maximum.x = std::max(box.m_maximum.x, pt.x);
+		b.m_maximum.y = std::max(box.m_maximum.y, pt.y);
+		b.m_maximum.z = std::max(box.m_maximum.z, pt.z);
+
+		return b;
+	}
+
+	Aabb::Aabb(const Point3& pt)
+	{
+		m_minimum = pt;
+		m_maximum = pt;
 	}
 }
--- src/raytracer/accelerator/aabb.h
+++ src/raytracer/accelerator/aabb.h
@@ -11,19 +11,23 @@ namespace raytracer
 	public:
 		Aabb() = default;
 
-		Aabb(const point3& a, const point3& b);
+		Aabb(const Point3& p);
 
-		[[nodiscard]] point3 minimum() const;
+		Aabb(const Point3& a, const Point3& b);
 
-		[[nodiscard]] point3 maximum() const;
+		[[nodiscard]] Point3 minimum() const;
+
+		[[nodiscard]] Point3 maximum() const;
 
 		[[nodiscard]] bool hit(const Ray& r, float t_min, float t_max) const;
 
 		static Aabb surroundingBox(const Aabb& box0, const Aabb& box1);
 
+		static Aabb unionPt(const Aabb& box, const Point3& pt);
+
 	private:
-		point3 m_minimum{};
-		point3 m_maximum{};
+		Point3 m_minimum{};
+		Point3 m_maximum{};
 	};
 
 }
