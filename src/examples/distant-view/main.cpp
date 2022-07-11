a6f825463b43962c810b8ca35740c8ce347ec784
Johan Solbakken <johan.solbakken@qt.io>
2022-07-11 09:23:30 +0200

    #2 update values in distant-view example

--- src/examples/distant-view/main.cpp
+++ src/examples/distant-view/main.cpp
@@ -7,17 +7,17 @@ raytracer::ref<raytracer::Hittable> createWorld()
 {
 	using namespace raytracer;
 
-	auto material_ground = createRef<Lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = createRef<Lambertian>(color(0.1, 0.2, 0.5));
+	auto material_ground = createRef<Lambertian>(Color(0.8, 0.8, 0.0));
+	auto material_center = createRef<Lambertian>(Color(0.1, 0.2, 0.5));
 	auto material_left = createRef<Dielectric>(1.5);
-	auto material_right = createRef<Metal>(color(0.8, 0.6, 0.2), 0.0);
+	auto material_right = createRef<Metal>(Color(0.8, 0.6, 0.2), 0.0);
 
 	auto world = createRef<HittableList>();
-	world->add<Sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground);
-	world->add<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center);
-	world->add<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left);
-	world->add<Sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left);
-	world->add<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right);
+	world->add<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground);
+	world->add<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center);
+	world->add<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left);
+	world->add<Sphere>(Point3(-1.0, 0.0, -1.0), -0.45, material_left);
+	world->add<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right);
 
 	return world;
 }
@@ -32,9 +32,10 @@ int main(int argc, char** argv)
 
 	// Camera
 	raytracer::CameraSpecification cameraSpec;
-	cameraSpec.vfov = 90.0f;
+	cameraSpec.vfov = 30.0f;
 	cameraSpec.aspect_ratio = aspect_ratio;
 	cameraSpec.lookFrom = { -2, 2, 1 };
+	cameraSpec.lookAt = { 0, 0.7, 0};
 
 	auto camera = raytracer::createRef<raytracer::Camera>(cameraSpec);
 
