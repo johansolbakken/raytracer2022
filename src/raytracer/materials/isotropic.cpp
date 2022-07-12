daf08de60221ea3d836544a24c7cc288ccb3afef
Johan Solbakken <johan.solbakken@qt.io>
2022-07-12 16:15:26 +0200

    Add isotropic material

---
+++ src/raytracer/materials/isotropic.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Johan Solbakken on 7/12/2022.
+//
+
+#include "isotropic.h"
+
+#include "raytracer/ray.h"
+
+#include "hittable/hit_record.h"
+
+#include "textures/texture.h"
+#include "textures/solidcolor.h"
+
+namespace raytracer
+{
+	Isotropic::Isotropic(Color c) : m_albedo(createRef<SolidColor>(c)) {}
+
+	Isotropic::Isotropic(ref<Texture> a) : m_albedo(std::move(a)) {}
+
+	bool Isotropic::scatter(const Ray& r_in, const hit_record& rec, Color& attenuation, Ray& scattered) const
+	{
+		scattered = Ray(rec.p, randomInUnitSpace(), r_in.time());
+		attenuation = m_albedo->value(rec.u, rec.v, rec.p);
+		return true;
+	}
+} // raytracer
---
+++ src/raytracer/materials/isotropic.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <utility>
+
+#include "material.h"
+#include "raytracer/base.h"
+
+namespace raytracer
+{
+	class Texture;
+
+	class SolidColor;
+
+	class Isotropic : public Material
+	{
+	public:
+		explicit Isotropic(Color c);
+
+		explicit Isotropic(ref<Texture> a);
+
+		bool scatter(const Ray& r_in, const hit_record& rec, Color& attenuation, Ray& scattered) const override;
+
+	public:
+		ref<Texture> m_albedo;
+	};
+
+} // raytracer
