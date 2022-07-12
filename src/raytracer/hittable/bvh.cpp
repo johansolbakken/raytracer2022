8f9504966d5ddf757fe0f1b827926afe114c52b7
Johan Solbakken <johan.solbakken@qt.io>
2022-07-12 16:14:29 +0200

    #3 separated components in compare function
     ~~~
     box_a.minimum()[axis] < box_b.maximum()[axis]
     ~~~
    
     had to become
    
     ~~~
     if (axis == 0) {
     	return a_min.x < b_min.x;
    } else if (axis == 1) {
     	return a_min.y < b_min.y;
    } else {
     	return a_min.z < b_min.z;
    }
     ~~~

--- src/raytracer/hittable/bvh.cpp
+++ src/raytracer/hittable/bvh.cpp
@@ -13,99 +13,65 @@
 namespace raytracer
 {
 
-	BvhNode::BvhNode(const HittableList& list, float time0, float time1)
+	BvhNode::BvhNode(const HittableList& list, double time0, double time1)
 			: BvhNode(list.objects(), 0, list.objects().size(), time0, time1)
 	{
 	}
 
 	BvhNode::BvhNode(const std::vector<ref<Hittable>>& src_objects,
-			size_t start, size_t end, float time0, float time1)
+			size_t start, size_t end, double time0, double time1)
 	{
 		auto objects = src_objects; // Create a modifiable array of the source scene objects
 
-		int axis = randomInt(0, 2);
-		auto comparator = (axis == 0) ? boxXCompare
-									  : (axis == 1) ? boxYCompare
-													: boxZCompare;
+		int axis = randomInt(0,2);
+		auto comparator = (axis == 0) ? box_x_compare
+									  : (axis == 1) ? box_y_compare
+													: box_z_compare;
 
 		size_t object_span = end - start;
 
-		if (object_span == 1)
-		{
-			m_left = m_right = objects[start];
-		}
-		else if (object_span == 2)
-		{
-			if (comparator(objects[start], objects[start + 1]))
-			{
-				m_left = objects[start];
-				m_right = objects[start + 1];
+		if (object_span == 1) {
+			left = right = objects[start];
+		} else if (object_span == 2) {
+			if (comparator(objects[start], objects[start+1])) {
+				left = objects[start];
+				right = objects[start+1];
+			} else {
+				left = objects[start+1];
+				right = objects[start];
 			}
-			else
-			{
-				m_left = objects[start + 1];
-				m_right = objects[start];
-			}
-		}
-		else
-		{
+		} else {
 			std::sort(objects.begin() + start, objects.begin() + end, comparator);
 
-			auto mid = start + object_span / 2;
-			m_left = createRef<BvhNode>(objects, start, mid, time0, time1);
-			m_right = createRef<BvhNode>(objects, mid, end, time0, time1);
+			auto mid = start + object_span/2;
+			left = createRef<BvhNode>(objects, start, mid, time0, time1);
+			right = createRef<BvhNode>(objects, mid, end, time0, time1);
 		}
 
 		Aabb box_left, box_right;
 
-		if (!m_left->boundingBox(time0, time1, box_left)
-			|| !m_right->boundingBox(time0, time1, box_right)
+		if (  !left->boundingBox(time0, time1, box_left)
+			  || !right->boundingBox(time0, time1, box_right)
 				)
-			std::cout << "No bounding box in bvh_node constructor.\n";
+			std::cerr << "No bounding box in bvh_node constructor.\n";
 
-		m_box = Aabb::surroundingBox(box_left, box_right);
+		box = Aabb::surroundingBox(box_left, box_right);
 	}
 
-	bool BvhNode::boundingBox(float time0, float time1, Aabb& output_box) const
+	bool BvhNode::boundingBox(double time0, double time1, Aabb& output_box) const
 	{
-		output_box = m_box;
+		output_box = box;
 		return true;
 	}
 
-	bool BvhNode::hit(const Ray& r, float t_min, float t_max, hit_record& rec) const
+	bool BvhNode::hit(const Ray& r, double t_min, double t_max, hit_record& rec) const
 	{
-		if (!m_box.hit(r, t_min, t_max))
+		if (!box.hit(r, t_min, t_max))
 			return false;
 
-		bool hit_left = m_left->hit(r, t_min, t_max, rec);
-		bool hit_right = m_right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+		auto hit_left = left->hit(r, t_min, t_max, rec);
+		bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
 
 		return hit_left || hit_right;
-	}
-
-	bool boxCompare(const ref<Hittable>& a, const ref<Hittable>& b, int axis)
-	{
-		Aabb box_a;
-		Aabb box_b;
-
-		if (!a->boundingBox(0, 0, box_a) || !b->boundingBox(0, 0, box_b))
-			std::cout << "No bounding box in bvh_node constructor.\n";
-
-		return box_a.minimum()[axis] < box_b.maximum()[axis];
-	}
-
-	bool boxXCompare(const ref<Hittable>& a, const ref<Hittable>& b)
-	{
-		return boxCompare(a, b, 0);
-	}
-
-	bool boxYCompare(const ref<Hittable>& a, const ref<Hittable>& b)
-	{
-		return boxCompare(a, b, 1);
-	}
-
-	bool boxZCompare(const ref<Hittable>& a, const ref<Hittable>& b)
-	{
-		return boxCompare(a, b, 2);
 	}
 }
--- src/raytracer/hittable/bvh.h
+++ src/raytracer/hittable/bvh.h
@@ -12,31 +12,55 @@ namespace raytracer
 	class BvhNode : public Hittable
 	{
 	public:
+	public:
 		BvhNode() = default;
 
-		BvhNode(const HittableList& list, float time0, float time1);
+		BvhNode(const HittableList& list, double time0, double time1);
 
-		BvhNode(const std::vector<ref<Hittable>>& src_objects,
-				size_t start, size_t end, float time0, float time1);
+		BvhNode(
+				const std::vector<ref<Hittable>>& src_objects,
+		size_t start, size_t end, double time0, double time1);
 
-		~BvhNode() override = default;
+		bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const override;
 
-		bool hit(const Ray& r, float t_min, float t_max, hit_record& rec) const override;
-
-		bool boundingBox(float time0, float time1, Aabb& output_box) const override;
+		bool boundingBox(double time0, double time1, Aabb& output_box) const override;
 
 	public:
-		ref<Hittable> m_left;
-		ref<Hittable> m_right;
-		Aabb m_box;
+		ref<Hittable> left;
+		ref<Hittable> right;
+		Aabb box;
 	};
 
-	bool boxCompare(const ref<Hittable>& a, const ref<Hittable>& b, int axis);
+	inline bool box_compare(const ref<Hittable> a, const ref<Hittable> b, int axis) {
+		Aabb box_a;
+		Aabb box_b;
 
-	bool boxXCompare(const ref<Hittable>& a, const ref<Hittable>& b);
+		if (!a->boundingBox(0,0, box_a) || !b->boundingBox(0,0, box_b))
+			std::cerr << "No bounding box in bvh_node constructor.\n";
 
-	bool boxYCompare(const ref<Hittable>& a, const ref<Hittable>& b);
+		auto a_min = box_a.minimum();
+		auto b_min = box_b.minimum();
 
-	bool boxZCompare(const ref<Hittable>& a, const ref<Hittable>& b);
+		if (axis == 0) {
+			return a_min.x < b_min.x;
+		} else if (axis == 1) {
+			return a_min.y < b_min.y;
+		} else {
+			return a_min.z < b_min.z;
+		}
+	}
+
+
+	inline bool box_x_compare (const ref<Hittable> a, const ref<Hittable> b) {
+		return box_compare(a, b, 0);
+	}
+
+	inline bool box_y_compare (const ref<Hittable> a, const ref<Hittable> b) {
+		return box_compare(a, b, 1);
+	}
+
+	inline bool box_z_compare (const ref<Hittable> a, const ref<Hittable> b) {
+		return box_compare(a, b, 2);
+	}
 }
 
