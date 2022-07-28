d2baec11edfe67075e1691ba436131ebcd4a320a
Johan Solbakken <johan.hvaler@outlook.com>
2022-07-28 10:32:14 +0200

    runtime-quant: add camera

---
+++ src/runtime-quant/camera.cpp
@@ -0,0 +1,46 @@
+#include "camera.h"
+
+#include "raytracer/ray.h"
+
+namespace qb
+{
+    Camera::Camera()
+    {
+        m_position = {0, -10, 0};
+        m_lookAt = {0, 0, 0};
+        m_up = {0, 1, 0};
+        m_length = 1;
+        m_horizontalSize = 1;
+        m_aspect = 1;
+    }
+
+    raytracer::Ray Camera::generateRay(double projScreenX, double projScreenY)
+    {
+        // projScreenX = u, projScreenY = v
+
+        // Compute the location of the screen in world coordinates
+        auto screenWorldCoordinate = m_screenCenter + (m_u * projScreenX) + (m_v * projScreenY);
+        auto ray = raytracer::Ray(
+            raytracer::Point3(m_position),
+            raytracer::Vector3(screenWorldCoordinate),
+            0);
+        return ray;
+    }
+
+    void Camera::updateGeometry()
+    {
+        // Compute vector from the camera position to the lookAt position
+        m_alignmentVector = glm::normalize(m_lookAt - m_position);
+
+        // Compute u and v
+        m_u = glm::normalize(glm::cross(m_alignmentVector, m_up));
+        m_v = glm::normalize(glm::cross(m_u, m_alignmentVector));
+
+        // Compute position of center of the screen
+        m_screenCenter = m_position + (m_length * m_alignmentVector);
+
+        // Modify u and v to match the size and aspect ratio
+        m_u = m_u * m_horizontalSize;
+        m_v = m_v * (m_horizontalSize / m_aspect);
+    }
+}
---
+++ src/runtime-quant/camera.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "vec.h"
+
+namespace raytracer
+{
+    class Ray;
+}
+
+namespace qb
+{
+
+    class Camera
+    {
+    public:
+        Camera();
+
+        void setPosition(const Point3 &pos) { m_position = pos; }
+        void setLookAt(const Point3 &pos) { m_lookAt = pos; }
+        void setUp(const Vector3 &up) { m_up = up; }
+        void setLength(double length) { m_length = length; }
+        void setHorizontalSize(double size) { m_horizontalSize = size; }
+        void setAspect(double aspect) { m_aspect = aspect; }
+
+        Point3 position() const { return m_position; }
+        Point3 lookAt() const { return m_lookAt; }
+        Vector3 up() const { return m_up; }
+        Vector3 u() const { return m_u; }
+        Vector3 v() const { return m_v; }
+        double length() const { return m_length; }
+        double horizontalSize() const { m_horizontalSize; }
+        double aspect() const { return m_aspect; }
+        Point3 screenCenter() const { return m_screenCenter; }
+
+        raytracer::Ray generateRay(double projScreenX, double projScreenY);
+
+        void updateGeometry();
+
+    private:
+        Point3 m_position;
+        Point3 m_lookAt;
+        Vector3 m_up;
+        double m_length;
+        double m_horizontalSize;
+        double m_aspect;
+
+        Vector3 m_alignmentVector;
+        Vector3 m_u;
+        Vector3 m_v;
+        Vector3 m_screenCenter;
+    };
+
+}
