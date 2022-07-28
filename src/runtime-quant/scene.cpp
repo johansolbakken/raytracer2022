a55e11b6ea370afa8310094d987d6c959ac7fee0
Johan Solbakken <johan.hvaler@outlook.com>
2022-07-28 13:34:23 +0200

    runtime-quant: update scene with light and sphere

--- src/runtime-quant/scene.cpp
+++ src/runtime-quant/scene.cpp
@@ -1,24 +1,109 @@
 #include "scene.h"
 
+#include <iostream>
+
+#include <raytracer/ray.h>
+#include <lights/pointlight.h>
+
 Scene::Scene()
 {
+    m_camera = new qb::Camera();
+    m_camera->setPosition({0, -10, 0});
+    m_camera->setLookAt({0, 0, 0});
+    m_camera->setUp({0, 0, 1});
+    m_camera->setAspect(16.0 / 9.0);
+    m_camera->setHorizontalSize(1);
+    m_camera->updateGeometry();
 
+    // Create Objects & lights
+    {
+        using namespace raytracer;
+
+        // Objects
+        m_objectList.push_back(createRef<ObjSphere>());
+
+        // Lights
+        auto pointLight = createRef<PointLight>();
+        pointLight->setPosition({5, -10, -5.0});
+        pointLight->setColor({255, 255, 255});
+
+        m_lightList.push_back(pointLight);
+    }
 }
 
-bool Scene::render(qb::Image* outputImage)
+Scene::~Scene()
 {
-    if (!outputImage) return false;
+    delete m_camera;
+}
 
+bool Scene::render(qb::Image *outputImage)
+{
+    if (!outputImage)
+        return false;
+
+    // Get the dimensions of the output image
     auto width = outputImage->width();
     auto height = outputImage->height();
 
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            double red = (static_cast<double>(x)/static_cast<double>(width)) * 255.0;
-            double green = (static_cast<double>(y)/static_cast<double>(height)) * 255.0;
-            outputImage->setPixel(x, y, red, green, 0.0);
+    // Loop over each pixel in our image
+    raytracer::Ray cameraRay;
+    raytracer::Point3 intPoint;
+    raytracer::Vector3 localNormal;
+    raytracer::Vector3 localColor;
+
+    double xScaleFact = 1.0 / (static_cast<double>(width) / 2.0);
+    double yScaleFact = 1.0 / (static_cast<double>(height) / 2.0);
+    double minDist = 1e6;
+    double maxDist = 0.0;
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            double normX = static_cast<double>(x) * xScaleFact - 1.0;
+            double normY = static_cast<double>(y) * yScaleFact - 1.0;
+
+            m_camera->generateRay(normX, normY, &cameraRay);
+
+            for (const auto &currentObject : m_objectList)
+            {
+                bool validIntersection = currentObject->testIntersections(cameraRay, intPoint, localNormal, localColor);
+
+                if (!validIntersection)
+                {
+                    outputImage->setPixel(x, y, 0.0, 0.0, 0.0);
+                    continue;
+                }
+
+                double intensity = 0.0;
+                raytracer::Color color;
+                bool validIllumination = false;
+                for (const auto &light : m_lightList)
+                {
+                    validIllumination = light->computeIllumination(intPoint, localNormal, m_objectList, currentObject, color, intensity);
+                }
+
+                double dist = glm::distance(intPoint, cameraRay.origin());
+
+                if (dist > maxDist)
+                    maxDist = dist;
+
+                if (dist < minDist)
+                    minDist = dist;
+
+                if (!validIllumination)
+                {
+                    outputImage->setPixel(x, y, 0.0, 0.0, 0.0);
+                    continue;
+                }
+
+                outputImage->setPixel(x, y, 255.0 * intensity, 0.0, 0.0);
+            }
         }
     }
+
+    std::cout << "minDist: " << minDist << std::endl;
+    std::cout << "maxDist: " << maxDist << std::endl;
 
     return true;
 }
--- src/runtime-quant/scene.h
+++ src/runtime-quant/scene.h
@@ -1,18 +1,26 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 #include <SDL.h>
 #include "image.h"
+#include "camera.h"
+#include <objects/objsphere.h>
+#include <lights/lightbase.h>
 
 class Scene
 {
 public:
     Scene();
+    ~Scene();
 
-    bool render(qb::Image* outputImage);
+    bool render(qb::Image *outputImage);
 
 private:
-
 private:
-
+    qb::Camera *m_camera;
+    
+    // List of objects in the scene
+    std::vector<raytracer::ObjectRef> m_objectList;
+    std::vector<raytracer::LightRef> m_lightList;
 };
